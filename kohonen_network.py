abb046cf93a2c7a3c74a78d798d922330c77583e
Eirik Folkestad <eirik.ek.folkestad@gmail.com>
2016-10-28 12:23:05 +0200

    neighborhood function added

--- kohonen_network.py
+++ kohonen_network.py
@@ -10,6 +10,7 @@ class KohonenNework:
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.__class__.weights = self.weights
+        self.winner_neuron_index = int()
 
     def start_training(self, input_cases):
 
@@ -17,27 +18,27 @@ class KohonenNework:
 
             for input_case in input_cases:
                 output_signals = self.integrate_and_fire(input_case)
-                winner_neuron_index = output_signals.tolist().index(min(output_signals))
-                self.apply_weight_delta(winner_neuron_index, input_case)
+                self.winner_neuron_index = output_signals.tolist().index(min(output_signals))
+                self.apply_weight_delta(input_case)
 
     def integrate_and_fire(self, input_case):
-        output_signals = np.apply_along_axis(self.inverse_euclidean_distance, 1, self.weights, input_case)
+        output_signals = np.apply_along_axis(self.euclidean_distance, 1, self.weights, input_case)
         return output_signals
 
-    def inverse_euclidean_distance(self, weight, input_case):
+    def euclidean_distance(self, weight, input_case):
         return abs(np.linalg.norm(input_case - weight))
 
-    def apply_weight_delta(self, neuron_index, input_case):
-        updated_weight = self.weight_delta(self.weights[neuron_index], input_case)
-        self.weights[neuron_index] = updated_weight.tolist()
+    def apply_weight_delta(self, input_case):
+        for weight_index in range(len(self.weights)):
+            updated_weight = self.weight_delta(self.weights[winner_neuron_index], input_case, neighborhood_function(weight_index))
+            self.weights[winner_neuron_index] = updated_weight.tolist()
 
-    def weight_delta(self, weight, input_case):
-        return np.array((weight)) + (self.learning_rate * (np.array(input_case) - np.array(weight)))
+    def weight_delta(self, weight, input_case, neighborhood_factor):
+        return np.array((weight)) + (self.learning_rate * neighborhood_factor * (np.array(input_case) - np.array(weight)))
 
-    # Smoothing kernel a.k.a neighborhood function
-    def hci(self, winner_index, current_index, learning_rate):
-        pass
+    def neighboorhood_function(self, current_index):
+        distance_from_winner = self.euclidean_distance(self.weights[self.winner_neuron_index], self.weights[current_index])
+        if distance_from_winner < 1:
+            distance_from_winner = 1
+        return 1/(pow(distance_from_winner, 2))
 
-#
-# kn = KohonenNework(5, 0.4)
-# kn.start_training([[1.0, 1.0]])
