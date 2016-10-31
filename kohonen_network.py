f7b0f92f5b82bd04b03878a594871978aae249e1
Ole Christer Selvig <ole.selvig@gmail.com>
2016-10-31 16:00:05 +0100

    Add exponetial and linear decay functions

--- kohonen_network.py
+++ kohonen_network.py
@@ -2,43 +2,66 @@ import numpy as np
 from random import shuffle
 
 
-class KohonenNework:
+class KohonenNetwork:
 
     weights = []
 
-    def __init__(self, number_of_weights, learning_rate, epochs):
+    def __init__(self, number_of_weights, learning_rate, epochs, learning_exp_decay=False, radius_exp_decay=False):
         self.weights = np.random.rand(number_of_weights, 2)
-        self.learning_rate = learning_rate
-        self.learning_rate_delta = learning_rate / epochs
-        self.epochs = epochs
         self.__class__.weights = self.weights
-        self.radius = 6
+
+        # Learning Iterations
+        self.epochs = epochs
+        self.current_epoch = int()
+
+        # Learning Rate
+        self.initial_learning_rate = learning_rate
+        self.current_learning_rate = self.initial_learning_rate
+        self.learning_rate_delta = learning_rate / epochs
+        self.learning_exp_decay = learning_exp_decay
+
+        # Radius
+        self.initial_radius = 7
+        self.current_radius = self.initial_radius
         self.radius_delta = 1
+        self.radius_exp_decay = radius_exp_decay
 
     def start_training(self, input_cases):
 
-        for _ in xrange(self.epochs):
+        for epoch in xrange(self.epochs):
+
+            self.current_epoch = epoch
 
             for input_case in input_cases:
                 output_signals = self.integrate_and_fire(input_case)
                 winner_index = output_signals.tolist().index(min(output_signals))
                 self.update_weights(winner_index, input_case)
 
-            if self.radius != 0:
-                self.radius -= self.radius_delta
-
-            self.learning_rate -= self.learning_rate_delta
+            self.adjust_radius()
+            self.adjust_learning_rate()
             shuffle(input_cases)
 
+    def adjust_radius(self):
+        if self.radius_exp_decay:
+            self.current_radius = int(round(self.initial_radius * pow(0.60, self.current_epoch)))
+        else:
+            self.current_radius = self.initial_radius - (self.current_epoch * (self.initial_radius / float(self.epochs)))
+
+    def adjust_learning_rate(self):
+        if self.learning_exp_decay:
+            self.current_learning_rate = self.initial_learning_rate * pow(0.9, self.current_epoch)
+        else:
+            self.current_learning_rate = self.initial_learning_rate - (self.current_epoch * (self.initial_learning_rate / float(self.epochs)))
+
     def integrate_and_fire(self, input_case):
-        output_signals = np.apply_along_axis(self.inverse_euclidean_distance, 1, self.weights, input_case)
+        output_signals = np.apply_along_axis(self.euclidean_distance, 1, self.weights, input_case)
         return output_signals
 
-    def inverse_euclidean_distance(self, weight, input_case):
+    def euclidean_distance(self, weight, input_case):
         return abs(np.linalg.norm(input_case - weight))
 
     def update_weights(self, winner_index, input_case):
-        weight_indices = [((winner_index + i) % len(self.weights)) for i in xrange(-self.radius, self.radius + 1)]
+        weight_indices = [((winner_index + i) % len(self.weights)) for i in xrange(-self.current_radius, self.current_radius + 1)]
         while len(weight_indices):
             current_index = weight_indices.pop()
             self.apply_weight_delta(current_index, input_case)
@@ -48,6 +71,6 @@ class KohonenNework:
         self.weights[current_index] = updated_weight.tolist()
 
     def calculate_weight_delta(self, current_weight, input_case, neighborhood_factor):
-        return np.asarray(current_weight) + (self.learning_rate * neighborhood_factor * (np.array(input_case) - np.array(current_weight)))
+        return np.asarray(current_weight) + (self.current_learning_rate * neighborhood_factor * (np.array(input_case) - np.array(current_weight)))
 
 
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from plot_elastic_ring import PlotElasticRing
 from tsp_data_parser import TSPDataParser
-from ann_dummy import ANNDummy
-from kohonen_network import KohonenNework
+from kohonen_network import KohonenNetwork
 import threading
 
 
 def start_kohonen_network():
     cities_normalized = TSPDataParser.parse_to_list('western_sahara', True)
-    kn = KohonenNework(len(cities_normalized), 0.5, 400)
+    kn = KohonenNetwork(len(cities_normalized), 0.5, 400, radius_exp_decay=True, learning_exp_decay=True)
     kn.start_training(cities_normalized)
 
 
@@ -21,6 +20,6 @@ def main():
         cities_normalized = TSPDataParser.parse_to_list('western_sahara', True)
         per = PlotElasticRing(cities_normalized)
         while True:
-            per.update_graph(list(KohonenNework.weights))
+            per.update_graph(list(KohonenNetwork.weights))
 
 main()
